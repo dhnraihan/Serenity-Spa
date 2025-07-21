8b19945ae0892b651b69ed9346abaa2f30036686
dhnraiha <raihanfahad123@gmail.com>
2025-07-21 18:10:14 +0600

    #

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/Serenity-Spa">
+    <Router basename="/">
       <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Header />
